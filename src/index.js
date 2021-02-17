9226aeea2d879de4985bd7fcded370e67ea1fc53
fenixoleg7 <fenixoleg7@gmail.com>
2021-02-17 21:47:51 +0200

    feat: add sorting function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  let matrix_new = [];
+
+  if(!matrix) return matrix_new;
+  if(matrix.length == 0) return matrix_new;
+  if(Array.isArray(matrix) == false) return matrix_new;
+
+    matrix.forEach(function(item, index) {
+      if((index+1) %2 !== 0){
+        item.sort((a, b) => a > b ? 1 : -1)
+        item.forEach(function(elem1){
+          matrix_new.push(elem1)
+        })  
+      }else{
+        item.sort((a, b) => a < b ? 1 : -1)
+        item.forEach(function(elem2){
+          matrix_new.push(elem2)
+        })
+      }
+    });
+    console.log('=========')
+    console.log(matrix_new)
+    console.log('=========')
+  return matrix_new
 }
